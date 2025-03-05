2f536273a3fbe70457d26b1fcf33ba8530c3a191
Tahil Bansal <Tahil.Bansal@target.com>
2025-03-06 01:04:11 +0530

    Update category model

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     title: {type: String , required: true},
     value: {type: String , required: true},
-    imageUrl: {type: String , required: true},
+    imageUrl: {type: String , required: false},
 });
 
 module.exports = mongoose.model('Category', categorySchema);
